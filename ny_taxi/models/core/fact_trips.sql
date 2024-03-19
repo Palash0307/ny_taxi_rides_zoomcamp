b881e53d1017c888734aa0334fef9e4de1c6bf03
palashaggarwal@gmail.com <palashaggarwal@gmail.com>
2024-03-19 03:44:24 +0000

    fact_trips

--- ny_taxi/models/core/fact_trips.sql
+++ ny_taxi/models/core/fact_trips.sql
@@ -1,29 +1,72 @@
 {{ config(materialized="table") }}
 
 with
-    
+    green_tripdata as (
+        select
+            tripid,
+            vendorid,
+            ratecodeid,
+            passenger_count,
+            pickup_locationid,
+            dropoff_locationid,
+            trip_distance,
+            trip_type,
+            fare_amount,
+            store_and_fwd_flag,
+            extra,
+            mta_tax,
+            tip_amount,
+            tolls_amount,
+            ehail_fee,
+            improvement_surcharge,
+            total_amount,
+            payment_type,
+            payment_type_description,
+            'Green' as service_type
+        from {{ ref("stg_green") }}
+    ),
     yellow_tripdata as (
-        select *, 'Yellow' as service_type from {{ ref("stg_yellow") }}
+        select
+            tripid,
+            vendorid,
+            ratecodeid,
+            passenger_count,
+            trip_distance,
+            trip_type,
+            fare_amount,
+            pickup_locationid,
+            dropoff_locationid,
+            store_and_fwd_flag,
+            extra,
+            mta_tax,
+            tip_amount,
+            tolls_amount,
+            ehail_fee,
+            improvement_surcharge,
+            total_amount,
+            payment_type,
+            payment_type_description,
+            'Yellow' as service_type
+        from {{ ref("stg_yellow") }}
     ),
     trips_unioned as (
-        
+        select *
+        from green_tripdata
+        union all
         select *
         from yellow_tripdata
     ),
     dim_zones as (select * from {{ ref("dim_zone") }} where borough != 'Unknown')
+
 select
     trips_unioned.tripid,
     trips_unioned.vendorid,
     trips_unioned.service_type,
     trips_unioned.ratecodeid,
-    trips_unioned.pickup_locationid,
     pickup_zone.borough as pickup_borough,
     pickup_zone.zone as pickup_zone,
-    trips_unioned.dropoff_locationid,
     dropoff_zone.borough as dropoff_borough,
     dropoff_zone.zone as dropoff_zone,
-    trips_unioned.pickup_datetime,
-    trips_unioned.dropoff_datetime,
     trips_unioned.store_and_fwd_flag,
     trips_unioned.passenger_count,
     trips_unioned.trip_distance,
@@ -38,6 +81,7 @@ select
     trips_unioned.total_amount,
     trips_unioned.payment_type,
     trips_unioned.payment_type_description
+
 from trips_unioned
 inner join
     dim_zones as pickup_zone on trips_unioned.pickup_locationid = pickup_zone.locationid
--- ny_taxi/models/staging/stg_yellow.sql
+++ ny_taxi/models/staging/stg_yellow.sql
@@ -3,7 +3,7 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+    row_number() over(partition by Cast(vendorid as integer), tpep_pickup_datetime) as rn
   from {{ source('staging','yellow') }}
   where vendorid is not null 
 )
